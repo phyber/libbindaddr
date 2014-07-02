d146c36d16d475eb61c222b7df6bafbc0f7e1e6d
David O'Rourke <david.orourke@gmail.com>
2014-07-02 15:36:26 +0100

    Remove info from top of source files since it's now in README.md

--- libbindaddr.h
+++ libbindaddr.h
@@ -1,19 +1,3 @@
-/*
- * libbindaddr.so
- *
- * Compile with: gcc -Wall -fPIC -shared -o libbindaddr.so libbindaddr.c -ldl
- * Usage: LD_PRELOAD=./libbindaddr.so LIBBINDADDR_V4="127.0.0.1" yourprogram
- *
- * After executing the above yourprogram should now bind to the address in
- * the LIBBINDADDR_V4 environment variable instead of the address it would
- * normally bind to.
- *
- * You may also set the LIBBINDADDR_V6 environment variable which will be used
- * if bind is used with an IPv6 address.
- *
- * Defining DEBUG when compiling will result in some (possibly) helpful
- * messages being printed to stderr if the library ends up returning early.
- */
 #define _GNU_SOURCE
 
 #include <stdlib.h>
